6ed7b1702e9cfb5025a90a3e21c340bf2f12c17d
prathamesh-pichkate <prathameshpichkate@gmail.com>
2025-02-01 09:04:53 +0530

    mongodb_connection done

---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+import dotenv from 'dotenv';
+dotenv.config();
+import mongoose from 'mongoose';
+console.log(process.env.MONGODB_URI);
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
